f27cf36deb7d5ff8f35023e34aceedf747a7cbd3
Herdson <hersfrancisco@gmail.com>
2026-06-04 17:14:48 -0600

    Agregando deploy

--- api-authors-books/index.js
+++ api-authors-books/index.js
@@ -187,10 +187,28 @@ app.patch('/authors/:id', async (req, res) => {
 });
 
 
+async function startServer() {
+  try {
+
+    await sequelize.authenticate();
+    console.log('Connection success');
+
+    await sequelize.sync();
+    console.log('Sync models');
+
+    app.listen(port, () => {
+      console.log(`Server listen on port ${port}`);
+    });
+
+  } catch (error) {
+
+    console.error('Connection fail', error);
+
+  }
+}
 
 if (require.main === module) {
   startServer();
 }
-
 
 module.exports = app;
