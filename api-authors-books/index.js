7beb6e258c4dff6ea1c1c2c4ac2c0462e8f23a44
Herdson <hersfrancisco@gmail.com>
2026-06-04 13:53:34 -0600

    ajustes

--- api-authors-books/index.js
+++ api-authors-books/index.js
@@ -294,25 +294,7 @@ app.patch('/authors/:id', async (req, res) => {
 
 });
 
-async function startServer() {
-  try {
 
-    await sequelize.authenticate();
-    console.log('Connection success');
-
-    await sequelize.sync();
-    console.log('Sync models');
-
-    app.listen(port, () => {
-      console.log(`Server listen on port ${port}`);
-    });
-
-  } catch (error) {
-
-    console.error('Connection fail', error);
-
-  }
-}
 
 if (require.main === module) {
   startServer();
