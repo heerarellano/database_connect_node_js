bdfdde6530db689aecbbd02d116e857cf1a00e55
Herdson <hersfrancisco@gmail.com>
2026-06-04 18:04:59 -0600

    Agregando ajuste v2

--- api-authors-books/index.js
+++ api-authors-books/index.js
@@ -186,9 +186,23 @@ app.patch('/authors/:id', async (req, res) => {
 
 });
 
- app.listen(port, () => {
+async function startServer() {
+
+  app.listen(port, () => {
     console.log(`Server listen on port ${port}`);
   });
+ 
+  try {
+    await sequelize.authenticate();
+    console.log('Connection success');
+
+    await sequelize.sync();
+    console.log('Sync models');
+
+  } catch (error) {
+    console.error('Connection fail', error);
+  }
+}
 
 if (require.main === module) {
   startServer();
